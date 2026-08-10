e1fb65ccbb6519ae112e08cd859651b12be84b97
kssw117-sudo <kssw117@gmail.com>
2026-08-10 17:25:19 +0300

    Update redeem-appsumo.js

--- api/redeem-appsumo.js
+++ api/redeem-appsumo.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -60,4 +60,4 @@ module.exports = async (req, res) => {
   } catch (err) {
     return res.status(500).json({ error: 'Ошибка сервера, попробуйте снова' });
   }
-};
+}
